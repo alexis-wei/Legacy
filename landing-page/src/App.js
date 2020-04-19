7a7d49b34bdf2de47b5505a1b80801e355358208
Alexis <qq_wei369@hotmail.com>
2020-04-19 02:10:17 -0400

    add icons

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import logo from "./images/logo-black.svg";
+import star from "./images/star-of-life.svg";
+import person from "./images/person.svg";
+import money from "./images/money.svg";
+import circle from "./images/circle-notch.svg";
 import pushToAirtable from './pushToAirtable';
 import "./App.css";
 
@@ -10,9 +14,14 @@ function sendFeedback(props) {
 
 function Subsection(props) {
   return (
-    <div>
-      <h1 class="medium">{props.title}</h1>
-      <p class="book">{props.body}</p>
+    <div className="side-by-side">
+      <div id="sub-logo" >
+      <img alt="" src={props.img} height="30" />
+      </div>
+      <div>
+        <h1 class="medium">{props.title}</h1>
+        <p class="book">{props.body}</p>
+      </div>
     </div>
   );
 }
@@ -101,13 +110,13 @@ class App extends React.Component {
                 <p class="medium">We’re a couple of international first-gen Berkeley students who know how scary it is to think about tuition and college.</p>
             </div>
             <div id="info" >
-              <Subsection title="This isn’t some evil predatory loan" body="We don’t need your credit score, 
+              <Subsection img={star} title="This isn’t some evil predatory loan" body="We don’t need your credit score, 
               and we won’t charge you interest; we just want to see your merits as they’re presented; today"/>
-              <Subsection title="One to one mentorship, just for you" body="We’ll find you a dedicated Berkeley 
+              <Subsection img={person} title="One to one mentorship, just for you" body="We’ll find you a dedicated Berkeley 
               alum working in your future dream industry to show you the ropes "/>
-              <Subsection title="We make money when you do" body="Our repayment method is based off your future earnings, 
+              <Subsection img={money} title="We make money when you do" body="Our repayment method is based off your future earnings, 
               so you can count on us to be in your corner helping you succeed"/>
-              <Subsection title="No commitments from day one" body="We just want to talk! This is an entirely new project 
+              <Subsection img={circle} title="No commitments from day one" body="We just want to talk! This is an entirely new project 
               and we want to make this experience as valuable as we can. Even if you don’t think this was meant for you, 
               sign up so we can have a chat!"/>
             </div>
